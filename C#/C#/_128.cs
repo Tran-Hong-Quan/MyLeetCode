71e19dcbaf8a1a494260aef0f6d94d31da04813e
Hồng Quân <thichlike1@gmail.com>
2024-05-09 09:29:21 +0700

    128

---
+++ C#/C#/_128.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _128
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            foreach (int n in nums)
+            {
+                if (dic.ContainsKey(n)) continue;
+                dic.Add(n, 1);
+            }
+
+            int max = 0;
+            foreach (int n in nums)
+            {
+                int k = n - 1;
+                while (dic.ContainsKey(k))
+                {
+                    dic[n]++;
+                    dic[k] = dic[k + 1] - 1;
+                    k--;
+                }
+                max = Math.Max(max, dic[n]);
+            }
+            return max;
+        }
+    }
+}
